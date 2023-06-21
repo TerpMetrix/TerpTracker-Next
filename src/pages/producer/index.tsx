6c339db4ce3a88ea80e842e947c4bef406c19d50
Hunter Heston <huntersamuelheston@gmail.com>
2023-06-21 16:25:39 -0700

    Display producers as cards

--- src/pages/producer/index.tsx
+++ src/pages/producer/index.tsx
@@ -1,11 +1,13 @@
 import { GetServerSideProps } from "next";
 import { prisma } from "@/server/db";
+import Link from "next/link";
 
 type Producer = {
   id: number;
   name: string;
   location: string;
   website: string;
+  bannerImage?: string;
 };
 
 type ProducersProps = {
@@ -16,16 +18,42 @@ type ProducersProps = {
 // Main exported component for Producer page
 export default function Producers({ producers }: ProducersProps) {
   return (
-    <div>
+    <div className="flex flex-wrap justify-center gap-10 py-20">
       {producers.map((producer) => (
-        <a key={producer.id} href={"/producer/" + String(producer.id)}>
-          <p>{producer.name}</p>
-        </a>
+        <Producer key={producer.id} producer={producer} />
       ))}
     </div>
   );
 }
 
+function Producer({ producer }: { producer: Producer }) {
+  return (
+    <Link href={`/producer/${producer.id}`}>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            className="h-48 w-full overflow-hidden object-cover object-center"
+            src={producer.bannerImage}
+            alt={"banner image of " + producer.name}
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title capitalize">
+            {producer.name}
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <p>{producer.location}</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Flower</div>
+            <div className="badge badge-outline">Concentrate</div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+// Fetch the data to render on the page
 export const getServerSideProps: GetServerSideProps<
   ProducersProps
 > = async () => {
@@ -39,6 +67,7 @@ export const getServerSideProps: GetServerSideProps<
           name: producer.name,
           location: producer.location,
           website: producer.website,
+          bannerImage: producer.bannerImage,
         })),
       },
     };
