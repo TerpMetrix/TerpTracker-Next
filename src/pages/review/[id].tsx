0fb53542f26faa08c7a756e87fedd00ab947b2f8
Hunter Heston <huntersamuelheston@gmail.com>
2023-06-20 21:35:06 -0700

    display the review

--- src/pages/review/[id].tsx
+++ src/pages/review/[id].tsx
@@ -1,7 +1,57 @@
-import { useRouter } from "next/router";
+import type { GetServerSideProps } from "next";
+import { prisma } from "@/server/db";
+import Link from "next/link";
 
-export default function Review() {
-  const router = useRouter();
-  const { id } = router.query;
-  return <div>A review: {id}</div>;
+type ReviewProps = {
+  review: {
+    id: number;
+    comment: string;
+    rating: number;
+    profileId: string | null;
+    strainId: number;
+  };
+  notFound?: boolean;
+};
+
+// Main component
+export default function Review({ review }: ReviewProps) {
+  return (
+    <div>
+      <p>Review id: {review.id}</p>
+      <p>{review.comment}</p>
+      <p>{review.profileId}</p>
+      <Link href={"/strain/" + String(review.strainId)}>
+        Strain id: {review.strainId}
+      </Link>
+    </div>
+  );
 }
+
+// Get server side props
+export const getServerSideProps: GetServerSideProps<ReviewProps> = async (
+  context
+) => {
+  const review = await prisma.review.findUnique({
+    where: {
+      id: Number(context.params?.id) || -1,
+    },
+  });
+
+  if (!review) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      review: {
+        id: review.id,
+        comment: review.comment,
+        rating: review.rating,
+        profileId: review.profileId,
+        strainId: review.strainId,
+      },
+    },
+  };
+};
