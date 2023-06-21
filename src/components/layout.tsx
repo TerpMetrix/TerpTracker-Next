3720ba97534e9095d00baa93220abf7c67b2cc97
Hunter Heston <huntersamuelheston@gmail.com>
2023-06-21 16:00:03 -0700

    Add Footer and remove bg color

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import { ReactNode } from "react";
 import Navigation from "./navigation";
+import { Footer } from "./footer";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <Navigation />
       <main>{children}</main>
+      <Footer></Footer>
     </>
   );
 }
