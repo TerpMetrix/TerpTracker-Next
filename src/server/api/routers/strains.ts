cbaeb786cce2c6948a5b0d17ee6edd05cd712d1d
Hunter Heston <huntersamuelheston@gmail.com>
2023-06-20 20:14:52 -0700

    Strains router if needed

---
+++ src/server/api/routers/strains.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+
+export const strainRouter = createTRPCRouter({
+  getStrain: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ input, ctx }) => {
+      const strain = await ctx.prisma.strain.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return { strain };
+    }),
+
+  getAllStrains: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.strain.findMany();
+  }),
+});
