8460648997a727e61b2c0288147e3d1f7c9d90a4
Sam S <sammi12006@gmail.com>
2023-07-05 12:26:14 -0600

    Finished redesign of homepage

---
+++ src/components/Card.tsx
@@ -0,0 +1,58 @@
+// /components/Card.tsx
+import React from 'react';
+import type { Strain, Producer } from '../pages/index'; // Import the Strain type from the strains page
+import Image from 'next/image';
+import Tag from '@/components/tag';
+
+type CardProps = {
+    data: Strain | Producer;
+};
+
+const Card: React.FC<CardProps> = ({ data }) => {
+    if ('THC' in data) {
+        // data is of type Strain
+        return (
+            <div className="flex flex-col h-64 card w-64 md:w-96 bg-base-100 transition-all hover:-translate-y-2 hover:bg-secondary shadow-lg shadow-gray-100/5 overflow-hidden">
+                {data.image && (<Image
+                    className="h-48 w-full overflow-hidden object-cover object-center"
+                    src={data.image}
+                    width={400}
+                    height={200}
+                    alt={"image of " + data.name}
+                />)}
+                <div className="badge badge-info mb-2 absolute top-3 right-3 uppercase">{data.productType}</div>
+                <div className="flex flex-col justify-between px-4 py-2">
+                    <p className="text-2xl mb-2 font-medium">{data.name}</p>
+                    <div className="flex flex-row gap-4 my-2">
+                        {data.tags.map((tag) => {
+                            return <Tag tag={tag} key={tag.id} />;
+                        })}
+                    </div>
+                    <p className="text-gray-500">{Math.floor(data.THC * 100)}% THC</p>
+                </div>
+            </div>
+        );
+    } else if ('bannerImage' in data) {
+        // data is of type Producer
+        return (
+            <div className="flex flex-col h-56 card w-64 md:w-96 bg-base-100 transition-all hover:-translate-y-2 hover:bg-secondary shadow-lg shadow-gray-100/5 overflow-hidden">
+                {data.bannerImage && (
+                    <Image
+                        className="h-48 w-full overflow-hidden object-cover object-center"
+                        src={data.bannerImage}
+                        width={400} height={200}
+                        alt={"image of " + data.name} />
+                )}
+                <div className="flex flex-col justify-between px-4 py-2">
+                    <p className="text-2xl mb-2 font-medium">{data.name}</p>
+                    <p className="text-gray-500">{data.location}</p>
+                    {/* ...render other properties of Producer */}
+                </div>
+            </div>
+        );
+    } else {
+        return <div>Unknown data type</div>;
+    }
+};
+
+export default Card;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,129 @@
 import Head from "next/head";
 import { Search } from "lucide-react";
+import Carousel from '@/components/Carousel';
+import { prisma } from "@/server/db";
+import type { GetServerSideProps } from "next";
 
-export default function Home() {
+export type Strain = {
+  id: number;
+  name: string;
+  batchDate: string;
+  THC: number;
+  productType: string;
+  producerId: number;
+  producerName: string;
+  producerBannerImage: string;
+  image: string;
+  tags: {
+    weight: number;
+    id: number;
+    color: string;
+    lean: number;
+    name: string;
+  }[];
+};
+
+export type Producer = {
+  id: number;
+  name: string;
+  bannerImage: string;
+  location: string;
+};
+
+type HomeProps = {
+  strains: Strain[];
+  producers: Producer[];
+};
+
+export default function Home({ strains, producers }: HomeProps) {
   return (
     <>
       <Head>
         <title>TerpTracker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center bg-gradient-to-b pt-[10em] sm:pt-[8em] lg:pt-[8em]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <div className="flex flex-row items-center gap-2">
-            <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-              <span className="bg-gradient-to-r from-red-600 via-green-600 to-blue-600 bg-clip-text px-1 tracking-tight text-transparent">
-                Terp
-              </span>
-              Tracker
-            </h1>
-          </div>
-          <div className="flex w-4/5 flex-row items-center gap-1 space-x-2 sm:w-1/2">
-            <input
-              type="text"
-              placeholder="Find what's next in weed"
-              className="input-base-content input-bordered input w-full border-white shadow-xl shadow-green-700/10"
-            />
-            <button className="btn border-0 bg-green-600 shadow-xl shadow-green-700/10 hover:bg-green-700">
-              <Search className="w-6 sm:w-full" />
-            </button>
-          </div>
-          <p className="italic">For the state of Colorado only right now!</p>
+      <main className="flex min-h-screen flex-col m-auto overflow-hidden space-y-8 w-11/12">
+        <div className="flex w-4/5 flex-row items-center gap-1 space-x-2 sm:w-1/2 mx-auto p-4">
+          <input
+            type="text"
+            placeholder="Find what's next in weed..."
+            className="input-base-content input-bordered input w-full border-white shadow-xl shadow-green-700/10"
+          />
+          <button className="btn border-0 bg-green-600 shadow-xl shadow-green-700/10 hover:bg-green-700">
+            <Search className="w-6 sm:w-full" />
+          </button>
         </div>
+        <Carousel title="🔥 Strains" data={strains} />
+        <Carousel title="🔥 Producers" data={producers} />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  try {
+    const strains = await prisma.strain.findMany(
+      {
+        include: {
+          producer: {
+            select: {
+              id: true,
+              name: true,
+              bannerImage: true,
+              location: true,
+            },
+          },
+          tags: {
+            select: {
+              weight: true,
+              tag: {
+                select: {
+                  id: true,
+                  name: true,
+                  color: true,
+                  lean: true,
+                },
+              },
+            },
+          }
+        },
+      },
+    );
+
+    const producers = await prisma.producer.findMany();
+
+    return {
+      props: {
+        strains: strains.map((strain) => ({
+          id: strain.id,
+          name: strain.name,
+          batchDate: strain.batchDate.toDateString(),
+          productType: strain.productType,
+          producerBannerImage: strain.producer.bannerImage,
+          THC: strain.THC,
+          producerId: strain.producer.id,
+          producerName: strain.producer.name,
+          image: strain.image,
+          tags: strain.tags.map((tag) => ({
+            weight: tag.weight,
+            id: tag.tag.id,
+            color: tag.tag.color,
+            lean: tag.tag.lean,
+            name: tag.tag.name,
+          })),
+        })),
+        producers: producers.map((producer) => ({
+          id: producer.id,
+          name: producer.name,
+          bannerImage: producer.bannerImage,
+          location: producer.location,
+        })),
+      },
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      notFound: true,
+    };
+  }
+};
