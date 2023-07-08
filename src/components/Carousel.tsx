175346ca68275e53370960e317b5893b46d53a73
Sam S <me@samsteingard.com>
2023-07-08 13:34:21 -0600

    Remodel homepage (#11)
    * Finished redesign of homepage
    
    * Added more info links
    
    * Added TerpTracker header back

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,35 @@
+// /components/Carousel.tsx
+import React from 'react';
+import type { Strain, Producer } from '../pages/index'; // Import the Strain type from the strains page
+import Card from './Card';
+
+
+interface CarouselProps {
+    title: string;
+    data: Strain[] | Producer[];
+}
+
+export const getItemId = (carouselName: string, index: number) =>
+    `${carouselName}-item-${index}`
+
+const Carousel: React.FC<CarouselProps> = ({ title, data }) => {
+    return (
+        <>  
+            <h2 className="text-2xl font-bold mb-2 text-left w-full ml-5 md:ml-24">{title}</h2>
+            <ul className="flex overflow-x-auto gap-0 snap-x scroll-pl-14 md:gap-6 snap-mandatory mb-2 motion-safe:scroll-smooth scroll-">
+                {data.map((item: Strain | Producer, index: number) => (
+                    <li
+                        id={getItemId(title, index)}
+                        key={index}
+                        className="shrink-0 snap-start snap-always m-5"
+                    >
+                        <Card data = {item} key={index} />
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
+}
+
+export default Carousel;
+
