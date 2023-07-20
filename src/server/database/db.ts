240a0de9fe38e32c9cbb715d4e228db96828f8bd
Hunter Heston <huntersamuelheston@gmail.com>
2023-07-20 10:18:39 -0700

    Reduce log level in dev environments

--- src/server/database/db.ts
+++ src/server/database/db.ts
@@ -1,15 +1,24 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { env } from "@/env.mjs";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+type LogLevel = "info" | "query" | "warn" | "error";
+
+const logLevelOptions: Record<string, LogLevel[]> = {
+  zero: [], // nothing will be logged
+  one: ["error"], // errors will be logged
+  two: ["warn", "error"], // errors and warnings will be logged
+  three: ["query", "warn", "error"], // queries, errors, and warnings will be logged
+  four: ["info", "query", "warn", "error"], // everything will be logged
+};
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    log: env.NODE_ENV === "development" ? logLevelOptions.two : ["error"],
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
