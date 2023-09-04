50ed21f190cc332633cfc8a5cea92ef866a11fcd
Sam S <sammi12006@gmail.com>
2023-09-03 18:46:24 -0600

    fix package-lock + profiles viewable w.o session

--- src/pages/profile/[profileName]/index.tsx
+++ src/pages/profile/[profileName]/index.tsx
@@ -56,83 +56,76 @@ export default function Profile({ profile, notFound }: ProfileProps) {
           <title>{profile.profileName} | TerpTracker</title>
         </Head>
         <div>
-          {session && (
-            <>
-              <div className="relative mx-auto my-2 flex h-fit h-fit w-4/5 flex-row items-center justify-center rounded-xl bg-slate-100 p-4 text-slate-800 md:space-x-6 lg:ml-16 lg:w-2/5">
-                <Image
-                  className="mr-4 h-1/4 w-1/4 rounded-full"
-                  src={profile.user.image ?? ""}
-                  alt="Profile Image"
-                  width={200}
-                  height={200}
-                />
-                <div className="flex flex-col">
-                  <div className="flex flex-row items-center justify-center space-x-4">
-                    <h1 className="text-center text-xl md:text-3xl">
-                      {profile.profileName}
-                    </h1>
-                    {/* if user is logged in, and the username matches the route id, show an edit profile button */}
-                    {session.user.id === profile.userId ? (
-                      <Link
-                        href="/profile/[profileName]/edit"
-                        as={`/profile/${profile.profileName || ""}/edit`}
-                      >
-                        <button className="btn-primary btn h-auto">Edit</button>
-                      </Link>
-                    ) : (
-                      <div className="mt-6 h-6 w-24"></div>
-                    )}
-                  </div>
-                  <div className="absolute left-3 top-3 place-content-end">
-                    <p className="badge badge-warning mb-0.5">Level 1</p>
-                  </div>
-                  <div className="mx-auto flex w-full flex-row content-end justify-evenly md:gap-10 md:pr-6 md:pt-5">
-                    <div className="flex flex-col place-content-end items-center">
-                      <p className="font-bold">
-                        {profile.upvotedStrains.length}
-                      </p>
-                      <p className="italic">Liked Strains</p>
-                    </div>
-                    <div className="flex flex-col place-content-end items-center">
-                      <p className="font-bold">{profile.reviews.length}</p>
-                      <p className="italic">Comments</p>
-                    </div>
-                  </div>
-                  {/* map terptags to tags component */}
-                  {/* <div className='flex flex-row justify-center items-center space-x-2'>
+          <div className="relative mx-auto my-2 flex h-fit h-fit w-4/5 flex-row items-center justify-center rounded-xl bg-slate-100 p-4 text-slate-800 md:space-x-6 lg:ml-16 lg:w-2/5">
+            <Image
+              className="mr-4 h-1/4 w-1/4 rounded-full"
+              src={profile.user.image ?? ""}
+              alt="Profile Image"
+              width={200}
+              height={200}
+            />
+            <div className="flex flex-col">
+              <div className="flex flex-row items-center justify-center space-x-4">
+                <h1 className="text-center text-xl md:text-3xl">
+                  {profile.profileName}
+                </h1>
+                {/* if user is logged in, and the username matches the route id, show an edit profile button */}
+                {session?.user.id === profile.userId ? (
+                  <Link
+                    href="/profile/[profileName]/edit"
+                    as={`/profile/${profile.profileName || ""}/edit`}
+                  >
+                    <button className="btn-primary btn h-auto">Edit</button>
+                  </Link>
+                ) : (
+                  <div className="mt-6 h-6 w-24"></div>
+                )}
+              </div>
+              <div className="absolute left-3 top-3 place-content-end">
+                <p className="badge badge-warning mb-0.5">Level 1</p>
+              </div>
+              <div className="mx-auto flex w-full flex-row content-end justify-evenly md:gap-10 md:pr-6 md:pt-5">
+                <div className="flex flex-col place-content-end items-center">
+                  <p className="font-bold">{profile.upvotedStrains.length}</p>
+                  <p className="italic">Liked Strains</p>
+                </div>
+                <div className="flex flex-col place-content-end items-center">
+                  <p className="font-bold">{profile.reviews.length}</p>
+                  <p className="italic">Comments</p>
+                </div>
+              </div>
+              {/* map terptags to tags component */}
+              {/* <div className='flex flex-row justify-center items-center space-x-2'>
                     {favTerps?.map((tag) => {
                       return <Tag tag={tag} key={tag.id} />;
                     })}
                   </div> */}
-                </div>
-                {/* horizontal element that has 3 numbers, one for number of liked strains, one for number of comments, and one that displays the rank of the account */}
-              </div>
+            </div>
+            {/* horizontal element that has 3 numbers, one for number of liked strains, one for number of comments, and one that displays the rank of the account */}
+          </div>
 
-              {/* display strains */}
-              <div className="mt-4 flex w-full flex-col justify-center md:max-h-[55vh] md:flex-row">
-                <Carousel
-                  title="Liked Strains"
-                  data={profile.upvotedStrains}
-                  renderItem={(strain) => <StrainCard strain={strain} />}
-                  getKey={(strain) => strain.name}
+          {/* display strains */}
+          <div className="mt-4 flex w-full flex-col justify-center md:max-h-[55vh] md:flex-row">
+            <Carousel
+              title="Liked Strains"
+              data={profile.upvotedStrains}
+              renderItem={(strain) => <StrainCard strain={strain} />}
+              getKey={(strain) => strain.name}
+            />
+            <VCarousel
+              className="h-[400px] w-full md:h-auto"
+              title="Comments"
+              data={profile.reviews}
+              renderItem={(review) => (
+                <CommentCard
+                  review={review}
+                  forProfile={true}
+                  className="max-w-[80vw] md:max-w-md"
                 />
-                <VCarousel
-                  className="h-[400px] w-full md:h-auto"
-                  title="Comments"
-                  data={profile.reviews}
-                  renderItem={(review) => (
-                    <CommentCard
-                      review={review}
-                      forProfile={true}
-                      className="max-w-[80vw] md:max-w-md"
-                    />
-                  )}
-                  getKey={(review) => review.strain.name}
-                />
-              </div>
-              {/* <button onClick={() => signOut()}>Sign out</button> */}
-            </>
-          )}
+              )}
+              getKey={(review) => review.strain.name}
+            />
+          </div>
         </div>
       </>
     );
